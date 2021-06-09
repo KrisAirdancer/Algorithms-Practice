9bade8b821de1f68a6b7c8e8a921fbbe0ca06f3c
Kris Airdancer <chris.scott.marston@gmail.com>
2021-06-09 16:35:10 -0600

    Added Model Solution

--- CTCI_Problems/chapter_1/CheckPermutation.java
+++ CTCI_Problems/chapter_1/CheckPermutation.java
@@ -9,8 +9,8 @@ public class CheckPermutation {
 		String testThree = "Nope!";
 		
 		// Testing checkPermutation_X
-		System.out.println("checkPermutation_3 matching (true): " + checkPermutation_1(testOne, testTwo));
-		System.out.println("checkPermutation_3 not matching (false): " + checkPermutation_1(testOne, testThree));
+		System.out.println("checkPermutation_ModelSolution matching (true): " + checkPermutation_ModelSolution(testOne, testTwo));
+		System.out.println("checkPermutation_ModelSolution not matching (false): " + checkPermutation_ModelSolution(testOne, testThree));
 		
 	}
 	
@@ -65,7 +65,7 @@ public class CheckPermutation {
 	}
 	
 	/**
-	 * Second attempt
+	 * Attempt 2
 	 * 
 	 * SOLUTION IDEA:
 	 * - Create a tracking array to count the number of instances of each character in the first string,
@@ -113,7 +113,49 @@ public class CheckPermutation {
 	}
 	
 	/**
-	 * First attempt
+	 * Attempt 1
+	 * 
+	 * RESTATEMENT OF PROBLEM:
+	 * - Evaluate two strings to determine if they are a permutation. That is, do they contain exactly
+	 * the same set of characters Ex. aabbcc is a permutation of abcabc
+	 * 
+	 * SOLUTION IDEA:
+	 * - Sort the two input strings then loop over both of them at the same time comparing the characters in
+	 * each one to each other. That is, string[index] == stringTwo[index]?
+	 * 
+	 * BETTER OPTION
+	 * - Loop over both strings keeping track of how many times each character shows up in each string in
+	 * separate tracking arrays (will need one tracking array for each string), then compare the values
+	 * in the tracking arrays to each other. If the tracking arrays are the same, the strings are permutations.
+	 * 
+	 * FULL SOLUTION DESCRIPTION:
+	 * - Declare and initialize two new tracking arrays
+	 * - for loop to loop over both strings and update their tracking arrays
+	 * 	- Can use extended ASCII for this - eASCII is 256 characters long
+	 * 
+	 * DEFINITIONS OF VARIABLES AND PARAMETERS:
+	 * - Method parameters: String inputOne and String inputTwo
+	 * 
+	 * PSEUDO-CODE SOLUTION:
+	 * - Declare two tracking arrays - trackingOne and trackingTwo
+	 * - Initiate for loop with index = 0
+	 * 	- trackingOne = inputOne[index]
+	 * 	- etc.
+	 * - for loop to loop over both tracking arrays and compare their values
+	 * 	- if block to check for equality (if not equal)
+	 * 		- return false
+	 * - return true if the entire for loop is traversed and we make it to this point - that means we've found no
+	 * unequal values
+	 * 
+	 * NOTES ON IMPLEMENTATION:
+	 * - This is the memory "intensive" implementation. It will take up a bit more memory because it requires that we
+	 * create two new arrays. However, if we went with the sort both strings, we would also have to create two new arrays
+	 * or Strings. Since String objects have an array that holds all of the chars in the String, the two will be roughly
+	 * the same in terms of memory constraints - we could go and calculate the difference, but they would be so close
+	 * I'll not do the math at this point. If memory is a necessary constraint, we can do the math to work out
+	 * which option is better.
+	 * - Because sorting arrays is slower than simply traversing two strings and two arrays once each (four total
+	 * traversals), going with the non-sorting option will both keep our memory usage low and our speed higher.
 	 */
 	public static boolean checkPermutation_1(String inputOne, String inputTwo) {
 		
@@ -137,50 +179,32 @@ public class CheckPermutation {
 		return true;
 	}
 
+	/**
+	 * Model Solution
+	 */
+	public static boolean checkPermutation_ModelSolution(String inputOne, String inputTwo) {
+		if (inputOne.length() != inputTwo.length()) {
+			return false;
+		}
+		
+		int[] count = new int[256]; // Assumption: extended ASCII
+		
+		for (int index = 0; index < inputOne.length(); index++) {
+			count[inputOne.charAt(index)]++; // Increment the corresponding index in the count array
+		}
+		
+		for (int index = 0; index < inputTwo.length(); index++){
+			count[inputTwo.charAt(index)]--; // Decrement the corresponding index in the count array
+			
+			// If a value drops below zero, strings are not permutations
+			if (count[inputTwo.charAt(index)] < 0) {
+				return false;
+			}
+		}
+		/*
+		 * If reaches this point, count has no negative values (< 0) and therefore no positive values (> 0) either.
+		 * All values in count are exactly zero.
+		 */
+		return true;
+	}
 }
-
-/*
- ********** ATTEMPT 1 **********
- * RESTATEMENT OF PROBLEM:
- * - Evaluate two strings to determine if they are a permutation. That is, do they contain exactly
- * the same set of characters Ex. aabbcc is a permutation of abcabc
- * 
- * SOLUTION IDEA:
- * - Sort the two input strings then loop over both of them at the same time comparing the characters in
- * each one to each other. That is, string[index] == stringTwo[index]?
- * 
- * BETTER OPTION
- * - Loop over both strings keeping track of how many times each character shows up in each string in
- * separate tracking arrays (will need one tracking array for each string), then compare the values
- * in the tracking arrays to each other. If the tracking arrays are the same, the strings are permutations.
- * 
- * FULL SOLUTION DESCRIPTION:
- * - Declare and initialize two new tracking arrays
- * - for loop to loop over both strings and update their tracking arrays
- * 	- Can use extended ASCII for this - eASCII is 256 characters long
- * 
- * DEFINITIONS OF VARIABLES AND PARAMETERS:
- * - Method parameters: String inputOne and String inputTwo
- * 
- * PSEUDO-CODE SOLUTION:
- * - Declare two tracking arrays - trackingOne and trackingTwo
- * - Initiate for loop with index = 0
- * 	- trackingOne = inputOne[index]
- * 	- etc.
- * - for loop to loop over both tracking arrays and compare their values
- * 	- if block to check for equality (if not equal)
- * 		- return false
- * - return true if the entire for loop is traversed and we make it to this point - that means we've found no
- * unequal values
- * 
- * NOTES ON IMPLEMENTATION:
- * - This is the memory "intensive" implementation. It will take up a bit more memory because it requires that we
- * create two new arrays. However, if we went with the sort both strings, we would also have to create two new arrays
- * or Strings. Since String objects have an array that holds all of the chars in the String, the two will be roughly
- * the same in terms of memory constraints - we could go and calculate the difference, but they would be so close
- * I'll not do the math at this point. If memory is a necessary constraint, we can do the math to work out
- * which option is better.
- * - Because sorting arrays is slower than simply traversing two strings and two arrays once each (four total
- * traversals), going with the non-sorting option will both keep our memory usage low and our speed higher.
- * 
- */
