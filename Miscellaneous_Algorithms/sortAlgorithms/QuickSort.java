272953b4da4a23e1b7984e07e7b47236fd8df22a
Kris Airdancer <chris.scott.marston@gmail.com>
2021-06-24 15:28:39 -0600

    Started building Quick Sort

--- Miscellaneous_Algorithms/sortAlgorithms/QuickSort.java
+++ Miscellaneous_Algorithms/sortAlgorithms/QuickSort.java
@@ -8,10 +8,92 @@ public class QuickSort {
 		
 		int[] test = {25, 57, 99, 27, 53, 45, 63, 92};
 		
-		quickSort_CTCI(test);
-		System.out.println(Arrays.toString(test));
+		quickSort_A2(test);
+		System.out.println("Input: [25, 57, 99, 27, 53, 45, 63, 92], Output: " + Arrays.toString(test));
 
 	}
+	
+	/**
+	 * Attempt 2 - My own implementation
+	 * 
+	 * SOLUTION IDEA:
+	 * 
+	 * FULL SOLUTION DESCRIPTION:
+	 * method header quickSort(int[] input, int startIndex, int endIndex)
+	 * Recursive Step
+	 * - Base Case: If input.length < 2, return
+	 * - Recursive Cases: if input.length >= 2,
+	 * 	- call quickSort(input, 0, pivot - 1) for the left side of the list
+	 * 	- call quickSort(input, pivot + 1, input.length) for the right side of the list 
+	 * Sorting Step
+	 * - Select the midpoint as the pivot point
+	 * - Swap the last item in the list with the pivot point
+	 * - Initialize two counters: firstLarger and searchIndex - initialize both to the first item in the list
+	 * - while loop that runs until searchIndex reaches the second to last item in the list -> searchIndex < input.length - 1 
+	 * 	- if searchIndex < pivot, swap searchIndex with firstLarger then increment first larger
+	 * 	- searchIndex++
+	 * 	- NOTE: if searchInex > pivot, do nothing.
+	 * - Once while loop completes,
+	 * 	- Swap pivot element with firstLarger element and sorting is complete
+	 * 
+	 * 
+	 */
+	public static void quickSort_A2(int[] input, int start, int end) {
+		
+		// BASE CASE: if input partition is of length one, return
+		if (end - start < 2) {
+			return;
+		}
+		
+		// SORT THE ELEMENTS
+		// Select a pivot point - in this case, the middle element is selected as the pivot
+		int pivotIndex = start + (end - start) / 2;
+		
+		// Swap the pivot element with the last item in the list to get the pivot out of the way for sorting - we will be using firstLargest to mark where the pivot should go
+		int temp = input[pivotIndex];
+		input[pivotIndex] = input[input.length - 1];
+		input[input.length - 1] = temp;
+		// The pivot has been moved, so now we need to reset the pivotIndex to where the pivot element is - in this case, the end of the list
+		pivotIndex = input.length - 1;
+		
+		int searchIndex = start;
+		int firstLarger = start;
+		
+		while (searchIndex < end) {
+			// If we find an element that is smaller than the pivot, we swap it with the firstLargest
+			if (input[searchIndex] < input[pivotIndex]) {
+				temp = input[searchIndex];
+				input[searchIndex] = input[firstLarger];
+				input[firstLarger] = temp;
+				
+				// Increment firstLarger to separate the smaller elements from the larger elements
+				firstLarger++;
+			}
+			
+			searchIndex++;
+		}
+		// Swap the pivot and firstLargest to put pivot back in place (pivot should still be the last element in the list)
+		temp = input[pivotIndex];
+		input[pivotIndex] = input[firstLarger];
+		input[firstLarger] = temp;
+		pivotIndex = firstLarger;
+		
+		
+		// RECURSIVE CASE:
+		// Call quickSort for the left half of the list
+		quickSort_A2(input, 0, pivotIndex - 1);
+		// Call quickSort for the right half of the list
+		quickSort_A2(input, pivotIndex + 1, input.length - 1);
+		
+	}
+	
+	/**
+	 * Driver for quickSort_A2
+	 */
+	public static void quickSort_A2(int[] input) {
+		quickSort_A2(input, 0, input.length - 1);
+	}
+	
 	
 	/**
 	 * Attempt 1 - CTCI Implementation
