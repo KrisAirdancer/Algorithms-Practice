9c08ebbee3d89ff8ae55a1aa0f6d56b40c187f37
Kris Airdancer <chris.scott.marston@gmail.com>
2021-07-06 09:21:01 -0600

    Completed LEET 13 Roman to Integer

--- LeetCode/problems_000_to_99/RomanToInteger_13.java
+++ LeetCode/problems_000_to_99/RomanToInteger_13.java
@@ -4,15 +4,75 @@ public class RomanToInteger_13 {
 
 	public static void main(String[] args) {
 		
-		System.out.println(romanToInt_A2("I"));
-		System.out.println(romanToInt_A2("II"));
-		System.out.println(romanToInt_A2("III"));
-		System.out.println(romanToInt_A2("IV"));
-		System.out.println(romanToInt_A2("IX"));
-		System.out.println(romanToInt_A2("LVIII"));
-		System.out.println(romanToInt_A2("MCMXCIV"));
+		System.out.println(romanToInt_A3("I") + ", ONE");
+		System.out.println(romanToInt_A3("II") + ", TWO");
+		System.out.println(romanToInt_A3("III") + ", THREE");
+		System.out.println(romanToInt_A3("IV") + ", FOUR");
+		System.out.println(romanToInt_A3("IX") + ", NINE");
+		System.out.println(romanToInt_A3("LVIII") + ", FIFTY-EIGHT");
+		System.out.println(romanToInt_A3("MCMXCIV") + ", NINETEEN-NINTY-FOUR");
 
 	}
+	
+	/**
+	 * Attempt 3 - July 6, 2021
+	 * 
+	 * SOLUTION OUTLINE:
+	 * - 
+	 * 
+	 */
+	public static int romanToInt_A3(String input) {
+		
+		int result = 0;
+		
+		if (input.length() < 2) {
+			result = romanCharToInt_A3(input.charAt(0));
+			return result;
+		}
+		
+		for (int index = 0; index < input.length(); index++) {
+			
+			int current = romanCharToInt_A3(input.charAt(index));
+			int next = 0;
+			if (index + 1 < input.length()) {
+				next = romanCharToInt_A3(input.charAt(index + 1));
+			}
+			
+			if (next > current) {
+				result += (next - current);
+				index++;
+			} else {
+				result += current;
+			}
+		}	
+		return result;
+	}
+	
+	/**
+	 * Helper method for A3
+	 */
+	public static int romanCharToInt_A3(char input) {
+		
+		switch (input) {
+			case 'I':
+				return 1;
+			case 'V':
+				return 5;
+			case 'X':
+				return 10;
+			case 'L':
+				return 50;
+			case 'C':
+				return 100;
+			case 'D':
+				return 500;
+			case 'M':
+				return 1000;
+			default:
+				return -1;
+		}
+	}
+	
 	
 	/**
 	 * Attempt 2 - July 5, 2021
