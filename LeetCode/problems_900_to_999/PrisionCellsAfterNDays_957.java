e0ea752b8630c20540b315ead2135de081dcd2b2
Chris Marston <chris.scott.marston@gmail.com>
2021-08-06 10:01:45 -0600

    Started working through solution for LEET 957

--- LeetCode/problems_900_to_999/PrisionCellsAfterNDays_957.java
+++ LeetCode/problems_900_to_999/PrisionCellsAfterNDays_957.java
@@ -12,6 +12,51 @@ public class PrisionCellsAfterNDays_957 {
 		System.out.println("Expected Output: [0, 0, 1, 1, 0, 0, 0, 0]");
 
 	}
+	
+	/**
+	 * Master Solution - August 6, 2021
+	 * 
+	 * Based on this solution: https://leetcode.com/problems/prison-cells-after-n-days/discuss/206205/Easy-to-understand-solution-Java-O(1)-memory-simulation-cycle.
+	 */
+	public static int[] prisionCells_A1(int[] cells, int days) {
+		
+		// Create array to hold the updated cell state - original array will hold previous state
+		int[] firstSimulation = new int[cells.length];
+		
+		for (int i = 1; i < 7; i++) { // Update cells that have neighbors of the same state
+			firstSimulation[i] = (cells[i - 1] == cells[i + 1] ? 1 : 0);
+		}
+		
+		// Copy simulation array to cells array 
+		for (int i = 0; i < cells.length; i++) {
+			cells[i] = firstSimulation[i];
+		}
+		
+		days -= 1; // Update days count
+		
+		int cycle = 1;
+		
+		while (days > 0) {
+			
+			int[] nextSimulation = new int[cells.length];
+			
+			for (int i = 1; i < 7; i++) {
+				nextSimulation[i] = (cells[i - 1] == cells[i + 1] ? 1 : 0);
+			}
+			
+			if (Arrays.equals(firstSimulation, nextSimulation)) {
+				days %= cycle;
+			}
+			
+			for (int i = 0; i < cells.length; i++) {
+				cells[i] = nextSimulation[i];
+			}
+			
+			cycle++;
+			days--;
+		}
+		return cells;
+	}
 
 	/**
 	 * Attempt 1 - August 5, 2021
