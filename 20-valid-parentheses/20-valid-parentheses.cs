bd2335b41a70daf29d72b997f1af49e6c68f4cc0
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-02-24 13:35:48 -0700

    Time: 119 ms (37.69%), Space: 36.7 MB (46.32%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public bool IsValid(string s) {
+        
+        if (s.Length == 1) { return false; }
+        
+        Stack<char> stack = new Stack<char>();
+        
+        for (int i = 0; i < s.Length; i++) {
+            
+            char token = s[i];
+            Console.WriteLine(token);
+            if (token == '(' || token == '[' || token == '{') {
+                stack.Push(token);
+                
+            } else { // token is closed paren or invalid character
+                
+                char last;
+                if (stack.Count > 0) {
+                    last = stack.Pop();
+                } else {
+                    return false;
+                }
+                
+                if (last == '(' && token != ')') { return false; }
+                else if (last == '[' && token != ']') { return false; }
+                else if (last == '{' && token != '}') { return false; }
+            }
+        }
+        if (stack.Count > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
