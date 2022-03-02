48738cf43f5e26cfcc852b8993fad7a34b982f81
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-03-02 10:48:10 -0700

    Time: 116 ms (92.61%), Space: 44.9 MB (5.12%) - LeetHub

--- 704-binary-search/704-binary-search.cs
+++ 704-binary-search/704-binary-search.cs
@@ -4,18 +4,7 @@ public class Solution {
         return Search(nums, target, 0, nums.Length - 1);
     }
     
-    private int Search(int[] nums, int target, int low, int high) {
-        
-        // Base case: low > high
-        
-        // Calculate midpoint
-        
-        // If target == nums[mid], return mid (mid is the element's index)
-        // If target < nums[mid], search left partition
-        // If target > nums[mid], search right partition
-        
-        // Else, return -1
-        
+    private int Search(int[] nums, int target, int low, int high) {       
         
         while (low <= high) {
             
