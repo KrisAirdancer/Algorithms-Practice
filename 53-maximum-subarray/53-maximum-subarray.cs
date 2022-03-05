e3bd2f57cb60ec7b79fc33aeeda4b828d55d2625
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-03-05 09:55:22 -0700

    Time: 260 ms (67.47%), Space: 47.7 MB (81.74%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cs
+++ 53-maximum-subarray/53-maximum-subarray.cs
@@ -1,22 +1,15 @@
 public class Solution {
+    
     public int MaxSubArray(int[] nums) {
-
-        // Loop over entire nums array to find the largest item in the list.
-            // This will be our minimum sum threshold
-            // Takes O(N) time
-        // Loop over the entire nums array,
-            // Add the current element to the sum
-            // If the current sum is larger than the maximum value, maxSoFar = sum
-            // If sum < largestValue, sum = 0 (might need to be if sum < 0)
-        // return maxSoFar
         
-        int maxValue = nums[0];
-        for (int i = 1; i < nums.Length; i++) {
-            if (nums[i] > maxValue) { maxValue = nums[i]; }
-        }
+        // int maxValue = nums[0];
+        // for (int i = 1; i < nums.Length; i++) {
+        //     if (nums[i] > maxValue) { maxValue = nums[i]; }
+        // }
         
-        int sum = 0; // May need to be set to 0
-        int maxSoFar = maxValue; // May need to be set to 0
+        int sum = 0;
+        // int maxSoFar = maxValue;
+        int maxSoFar = nums[0];
         for (int i = 0; i < nums.Length; i++) {
             
             sum += nums[i];
