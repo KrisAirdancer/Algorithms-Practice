b5aa20efaa4056698e89ba06656c2194c6eb132a
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-03-16 08:20:48 -0600

    Time: 264 ms (6.58%), Space: 40.7 MB (69.94%) - LeetHub

---
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cs
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    
+    // <summary>
+    /// Recursive solution
+    /// 
+    /// In-Order Traversal:
+    /// - N.Left
+    /// - Use N
+    /// - N.Right
+    /// </summary>
+    public IList<int> InorderTraversal(TreeNode root) {
+        
+        if (root == null) {
+            return new List<int>();
+        }
+        
+        IList<int> orderedNodes = new List<int>();
+        return InorderTraversalRecursive(orderedNodes, root);
+        
+    }
+    
+    public IList<int> InorderTraversalRecursive(IList<int> orderedNodes, TreeNode start) {
+        // Console.WriteLine($"current: {start.val}");
+            
+        if (start.left != null) {
+            InorderTraversalRecursive(orderedNodes, start.left);
+        }
+
+        orderedNodes.Add(start.val);
+
+        if (start.right != null) {
+            InorderTraversalRecursive(orderedNodes, start.right);
+        }
+        return orderedNodes;
+    }
+}
