ce63385bea134dd4cbae5a416488cfce28ee1914
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-05-01 09:01:29 -0600

    Time: 39 ms (17.91%), Space: 22.5 MB (52.43%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cs
+++ 191-number-of-1-bits/191-number-of-1-bits.cs
@@ -7,6 +7,13 @@ public class Solution {
     /// increment a count. After iteration, return the count. Use the right bit shift operator, 
     /// >>, to cycle through the bits.
     /// 
+    /// Chose not to use the "fancy" bit manipulation trick of comparing n and n - 1 bitwise.
+    /// This solution might be faster than the one I implemented, but it is more confusing
+    /// and both are O(1) time, so there is no real gain. However, the "fancy"/"faster"
+    /// option is less maintainable because it is more difficult to understand and it isn't
+    /// faster in any meaningful way. Maybe for an algs competiton, but not for software or
+    /// other projects.
+    /// 
     /// PERFORMANCE:
     /// Time Complexity: O(1) b/c always looping over 32 bits
     /// Memory Complexity: O(1)
@@ -18,6 +25,10 @@ public class Solution {
     ///     Shift the bits in n to the right by one (n = n >> 1)
     /// Return count
     /// 
+    /// REFERENCES: 
+    /// Explanation of the "fancy" method: https://leetcode.com/problems/number-of-1-bits/discuss/1044775/Python-n-and-(n-1)-trick-%2B-even-faster-explained
+    /// Worked through this solution to understand the solution and implement my solution: https://leetcode.com/problems/number-of-1-bits/discuss/1044799/C%2B%2B-or-Bit-Manipulation-or-4-Approaches-or-Beats-100-0ms-O(1)
+    /// 
     /// </summary>
     public int HammingWeight(uint n) {
         
@@ -25,10 +36,12 @@ public class Solution {
         
         while (n > 0) {
             
+            // If the bit in the first (right most) position is a 1
             if ((n & 1) == 1) {
                 count++;
             }
             
+            // Shift the bits to the right by one
             n = (n >> 1);
         }
         
