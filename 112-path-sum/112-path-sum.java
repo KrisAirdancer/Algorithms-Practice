e48451df066354a8ddc1135a4f9a2cb3e6c8bd41
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-08-29 10:20:08 -0600

    Time: 1 ms (63.31%), Space: 43.4 MB (74.56%) - LeetHub

---
+++ 112-path-sum/112-path-sum.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    /**
+     * IDEA:
+     * 
+     * OUTLINE:
+     * - If targetSum == 0 && at leaf node, return true
+     * - If targetSum == 0 && not at leaf node, return false
+     * - If targetSum < 0, return false.
+     * - hasPathSum(root.left, targetSum - root.val);
+     * - hasPathSum(root.right, targetSum - root.val);
+     */
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        
+        if (root == null) {
+            return false;
+        }
+            
+        if (targetSum - root.val == 0 && (root.left == null && root.right == null)) {
+            return true;
+        }
+        
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+        
+//         if (targetSum == 0 && root.left == null && root.right == null) {
+//             return true;
+//         } else if (targetSum == 0 && (root.left != null || root.right != null)) {
+//             return false;
+//         } else if (targetSum < 0) {
+//             return false;
+//         }
+        
+//         hasPathSum(root.left, targetSum - root.val);
+//         hasPathSum(root.right, targetSum - root.val);
+    }
+}
