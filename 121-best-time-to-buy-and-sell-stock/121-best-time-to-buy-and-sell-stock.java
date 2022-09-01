6e3c55c2ceddfbba7b702ef43246f2d066ab37d6
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-09-01 07:58:13 -0600

    Time: 1 ms (100.00%), Space: 58.8 MB (95.92%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,10 +1,11 @@
 class Solution {
     
     /**
-     * IDEA: Use two pointers to loop over the array. The left pointer keeps track of the buy point,
+     * IDEA:
+     * - Use two pointers to loop over the array. The left pointer keeps track of the buy point,
      * while the left pointer looks for the optimal sell point. For each element visited by the right
      * pointer, the difference between that value and hte value at the left pointer is calculated.
-     * If the new maxProffit (difference) is greater than one seen before, update maxProfit with the
+     * If the new maxProfit (difference) is greater than one seen before, update maxProfit with the
      * new value. Additionally, for each element visited by the right pointer, if the value at the right
      * pointer is less than the value at the left poiner, move the left pointer to the position of
      * the right pointer.
@@ -15,7 +16,9 @@ class Solution {
         int left = 0;
         
         for (int right = 0; right < prices.length; right++) {
+            
             int currentProfit = prices[right] - prices[left];
+            
             if (currentProfit > maxProfit) {
                 maxProfit = currentProfit;
             }
