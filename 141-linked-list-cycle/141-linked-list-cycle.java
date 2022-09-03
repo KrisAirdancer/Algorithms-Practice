73f5fad9dcbf17d394794a8690fec9ae9f223eaf
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-09-03 08:01:34 -0600

    Time: 0 ms (100.00%), Space: 46.1 MB (34.02%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    
+    /**
+     * IDEA:
+     * - Use two pointers to loop over the linked list. One pointer moves at twice the speed of the other.
+     * If the faster pointer catches up with the slower pointer, there is a cycle. If the faster pionter reaches
+     * a `null` node, there isn't a cycle.
+     */
+    public boolean hasCycle(ListNode head) {
+        
+        if (head == null) { return false; }
+        
+        // if (head == null || head.next == null) { return false; }
+        // else if (head.next == head) { return true; }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (fast != null) {
+            
+            // fast catches slow, there is a loop.
+            if (fast == slow) {
+                return true;
+            }
+            
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                fast = fast.next;
+            }
+            
+            slow = slow.next;
+        }
+        
+        return false;
+    }
+}
