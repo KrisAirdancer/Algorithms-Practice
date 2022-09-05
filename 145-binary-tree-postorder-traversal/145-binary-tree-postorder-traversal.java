8d72ada9aacf6a5c44d2b269d13dd241648fec38
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-09-05 08:08:47 -0600

    Time: 0 ms (100.00%), Space: 40.6 MB (89.09%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -19,6 +19,13 @@ class Solution {
      * IDEA:
      * - Use a recursive Postorder Traversal to visit every node and add them to an arraylist,
      * then return the arraylist.
+     * 
+     * PERFORMANCE:
+     * - O(N) time complexity b/c the alg only visits each of the N nodes once.
+     * - O(N) memory complexity in worst case b/c tree could be 100% unbalanced and be a binary stick. Therefore,
+     * the recursion would require a maximum of N calls to be on the stack at the same time to reach the
+     * bottom of the tree. If the tree is perfectly balanced, O(logN) memory complexity for the recursion,
+     * but O(N) to store the tree, so still O(N) overall.
      */
     public List<Integer> postorderTraversal(TreeNode root) {
         
