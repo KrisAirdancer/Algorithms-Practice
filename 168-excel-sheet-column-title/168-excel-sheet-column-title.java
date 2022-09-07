1e1aaa9e3c545599ce94ce51981e5964138d080f
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-09-07 10:22:02 -0600

    Time: 0 ms (100.00%), Space: 41.1 MB (70.93%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.java
@@ -7,7 +7,19 @@ class Solution {
      * to the rightmost character in the column name. However, this would require that we
      * write out a chunk of logic with 26 cases to assign each value to a character. Instead,
      * we can use ASCII values to cast the value to a character, but this will require that
-     * we shift the values slightly.
+     * we shift the values slightly to accomodate the fact that ASCII 'A' starts at 65 and 
+     * the mod operator will return a 1 that we want to correspond to 'A'. To do this, we
+     * reduce the column number by one as an offset then conduct the modulus operation.
+     * We can repeat this process until columnNumber == 0;
+     * 
+     * Performance:
+     * - O(N) time complexity - By using StringBuilder to build the columnName and by using .append() and
+     * .reverse() to build the string (as opposed to using .insert() alone), the alg only
+     * loops over the output string twice for any given input. Thus, O(N).
+     * - O(N) space complexity - Because the StringBuilder stores the string as an array of
+     * characters, the space complexity will be O(N), where N is comumnNumber / 26. That is,
+     * for every multiple of 26 in columnNumber, one additional character will be added
+     * to the StringBuilder array.
      */
     public String convertToTitle(int columnNumber) {
         
