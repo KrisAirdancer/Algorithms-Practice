ecf4f19bbf1b7acc066aa87cb7328196e1e39481
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-09-09 10:02:41 -0600

    Time: 2 ms (80.90%), Space: 42.7 MB (55.36%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.java
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.java
@@ -1,4 +1,18 @@
 class Solution {
+    
+    /**
+     * IDEA:
+     * - For each character in columnTitle (starting with the rightmost character), convert it into
+     *   its ASCII value. From here, add 1 to the ASCII value to match the 1-based indexing used in this
+     *   version of Excel. Then, multiply that value by 26^x, where x is the zero-based index of the 
+     *   character in the list. Repeat this for each of the characters in columnTitle to get the final
+     *   columnNumber.
+     * 
+     * PERFORMANCE:
+     * - O(N) time complexity - We only loop over the input string once and all of the operations performed
+     *   on the characters of the string are constant, O(1).
+     * - O(N) memory complexity - we only store the input array and don't duplicate any data, so O(N).
+     */
     public int titleToNumber(String columnTitle) {
         
         int columnNumber = 0;
