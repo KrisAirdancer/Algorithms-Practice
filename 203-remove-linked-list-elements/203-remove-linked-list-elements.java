0e8813740f4bf09ad96b3195538143880added4a
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-09-11 08:31:49 -0600

    Time: 4 ms (5.83%), Space: 49.7 MB (12.71%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.java
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.java
@@ -9,25 +9,36 @@
  * }
  */
 class Solution {
+    
+    /**
+     * PERFORMANCE:
+     * - O(N) time complexity - We iterate over each node in the list only once; therefore, O(N).
+     * - O(N) space complexity - We don't duplicate the list, so we only need to store the original. Thus, O(N).
+     * 
+     * SOURCES:
+     * - I worked through this solution/guide to reach my solution: https://leetcode.com/problems/remove-linked-list-elements/discuss/1572932/Java-Three-Simple-and-Clean-solutions-w-Explanation-or-Iterative-and-Recursive-or-Beats-100 
+     */
     public ListNode removeElements(ListNode head, int val) {
         // If list is empty
         if (head == null) {
             return null;
         }
         
-        ListNode newHead = new ListNode();
-        newHead.next = head;
+        ListNode tempHead = new ListNode();
+        // We add a new node to the beginning of the LinkedList to allow us to easily check and remove the first node (head) if head.val == val
+        tempHead.next = head;
         
-        ListNode currentNode = newHead;
+        ListNode currentNode = tempHead;
         
         while (currentNode != null) {
+            
             if (currentNode.next != null && currentNode.next.val == val) {
                 currentNode.next = currentNode.next.next;
             } else { // We don't iterate formally if we remove a node because removing a node effectively moves us forward by one.
                 currentNode = currentNode.next;
             }
         }
-        
-        return newHead.next;
+        // We return the node after tempHead b/c tempHead was just a temporary node to make it easy to remove the head node if needed. The real head is tempHead.next
+        return tempHead.next;
     }
 }
