effb1e54afe95b1858cf08a97ea85005d42f8c98
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-10-11 11:03:16 -0600

    Time: 107 ms (5.37%), Space: 29.3 MB (7.83%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cs
@@ -0,0 +1,50 @@
+public class Solution {
+    
+    /**
+     * IDEA:
+     * - Per the problem statement, we have two potential cases:
+     *       1) n reduces to 1
+     *       2) The n does not reduce to 1 and instead gets stuck in a cycle
+     * - Use a loop to loop until n == 1 or until we're stuck in a loop. Use
+     *   a second loop (inside the first) to pull all of the digits off of 
+     *   n, square them, and then sum the results.
+     * 
+     * PERFORMANCE: 
+     * - Time Complexity: 
+     * - Memory Complexity: 
+     * 
+     */
+    public bool IsHappy(int n) {
+        
+        HashSet<int> seenNums = new HashSet<int>();
+        
+        while (n != 1) { // Check if n has reduced to 1
+            
+            if (seenNums.Contains(n)) { return false; } // Check if we're in a loop
+            
+            seenNums.Add(n);
+            
+            // Calculate next value
+            
+            int sum = 0;
+            
+            while (n != 0) {
+                               
+                int digit = n % 10;
+                n = n / 10;
+                
+                Console.WriteLine($"n: {n}, digit: {digit}");
+                
+                // sum += Math.Pow(n % 10, 2);
+                sum += (digit * digit);
+                
+                Console.WriteLine($"sum: {sum}");
+            }
+            
+            n = sum;
+        }
+        
+        
+        return true;
+    }
+}
