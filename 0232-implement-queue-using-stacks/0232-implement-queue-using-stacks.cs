9b87fdae95f4c42597e0e1b1be8df5ee4a8dbe8a
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-10-15 09:11:20 -0600

    Time: 221 ms (5.19%), Space: 39.2 MB (34.94%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cs
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cs
@@ -6,11 +6,20 @@
  *   Pop()/Peek() is called, don't move all items, instead, return the top item on outputStack.
  * 
  * EDGE CASES:
- * - Can't think of any at the moment
+ * - Pop()/Peek() is called when the outputQueue is empty.
  * 
  * PERFORMANCE:
- * - Time Complexity: 
- * - Memory Complexity: 
+ * - Time Complexity: O(1) amortized - Adding and removing items to/from the queue is usually
+ *                    done in a single Push()/Pop() call on the backing Stacks. However, 
+ *                    when the outputStack is empty, all elements from the inputStack are
+ *                    copied over to the outputStack. This operation takes O(N) time, but
+ *                    because it only happens occasionally, the majority of the operations
+ *                    on the queue are done in O(1) time. On average, operations on this
+ *                    queue are O(1).
+ * - Memory Complexity: O(N) - Since we are only ever storing the elements that are added
+ *                      to the queue, a list of N elements, the memory complexity of this
+ *                      queue is O(N).
+ * 
  */
 public class MyQueue {
     
@@ -29,26 +38,21 @@ public class MyQueue {
     
     public int Pop() {
         if (outputStack.Count == 0) {
-            
             while (inputStack.Count > 0) {
                 outputStack.Push(inputStack.Pop());
             }
-            
             return outputStack.Pop();
             
         } else {
-            
             return outputStack.Pop();
         }
     }
     
     public int Peek() {
         if (outputStack.Count == 0) {
-            
             while (inputStack.Count > 0) {
                 outputStack.Push(inputStack.Pop());
             }
-            
             return outputStack.Peek();
             
         } else {
