eac7d25e493602ea3c331f958e3aae59c32d3df7
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-10-21 09:07:41 -0600

    Time: 150 ms (25.72%), Space: 38.6 MB (75.90%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -5,7 +5,6 @@ public class Solution {
         if (s.Length != t.Length) {
             return false;
         }
-        Console.WriteLine("Here 1");
         
         Dictionary<char, int> sFreq = new Dictionary<char, int>();
         Dictionary<char, int> tFreq = new Dictionary<char, int>();
@@ -14,22 +13,18 @@ public class Solution {
         for (int i = 0; i < s.Length; i++) {
             
             char sCurrent = s[i];
-            Console.WriteLine($"sCurrent: {sCurrent}");
             
             if (sFreq.ContainsKey(sCurrent)) {
                 sFreq[sCurrent]++;
             } else {
                 sFreq.Add(sCurrent, 1);
             }
-            Console.WriteLine($"sFreq[{sCurrent}]: {sFreq[sCurrent]}");
         }
-        Console.WriteLine("Here 2");
         
         // Populate tFreq
         for (int i = 0; i < t.Length; i++) {
             
             char tCurrent = t[i];
-            Console.WriteLine($"tCurrent: {tCurrent}");
             
             if (tFreq.ContainsKey(tCurrent)) {
                 tFreq[tCurrent]++;
@@ -37,21 +32,16 @@ public class Solution {
                 tFreq.Add(tCurrent, 1);
             }
         }
-        Console.WriteLine("Here 3");
         
         // Compare characrer frequencies
-        for (int i = 0; i < s.Length; i++) {
-            Console.WriteLine($"i: {i}");           
+        for (int i = 0; i < s.Length; i++) { 
             
             char current = s[i];
-            Console.WriteLine($"current: {current}");
             
             if (!sFreq.ContainsKey(current) || !tFreq.ContainsKey(current) || sFreq[current] != tFreq[current]) {
-                // Console.WriteLine($"sFreq: {sFreq[current]}, tFreq: {tFreq[current]}");
                 return false;
             }
         }
-        Console.WriteLine("Here 4");
         
         return true;
     }
