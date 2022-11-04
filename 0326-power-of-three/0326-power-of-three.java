2bfa9de85dd794e499223193f1cdf5e7f6e7d112
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-11-04 08:54:51 -0600

    Time: 31 ms (13.14%), Space: 47.6 MB (20.14%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,5 +1,9 @@
 class Solution {
     
+    /**
+     * IDEA:
+     * - Apply logarithm rules to determine if n is a power of 3 or not.
+     */
     public boolean isPowerOfThree(int n) {
         
         return ((Math.log10(n) / Math.log10(3)) % 1) == 0;
