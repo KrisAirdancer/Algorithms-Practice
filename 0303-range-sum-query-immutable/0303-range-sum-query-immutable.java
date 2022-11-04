15bccafe20b7f111b14b05cb396a4bcf0ba08c50
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-11-03 19:35:03 -0600

    Time: 113 ms (5.11%), Space: 48.7 MB (75.49%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -2,6 +2,18 @@
  * IDEA:
  * - Loop over nums starting at index left and go until index right. On each loop,
  *   add the value at nums[i] to the result. Return the result.
+ * 
+ * PERFORMANCE (of sumRange()):
+ * - Time Complexity: O(N) - Where N is the difference between left and right. That is,
+ *                    the length of the range to be summed.
+ * - Memory Complexity: O(1) - Only a few variables need to be stored. No data needs to
+ *                      be copied.
+ * 
+ * PERFORMANCE (of multiple calls to sumRange()):
+ * - Time Complexity: O(N) - Still O(N). Since a single call to sumRange() is O(N), M calls
+ *                    to sumRange() wil be M * O(N), which is just O(N).
+ * - Memory Complexity: O(1) - Same as a single call to sumRange(). Only a few variables
+ *                      need to be stored. No data needs to be copied.
  */
 class NumArray {
 
