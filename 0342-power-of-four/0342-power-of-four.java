2a3198cf5cf2d9b5316030019d90e56888cd2c95
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-11-07 09:38:20 -0700

    Time: 3 ms (10.37%), Space: 40.8 MB (78.95%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,11 +1,22 @@
 class Solution {
     
+    /**
+     * IDEA:
+     * - Since n = 4^x returns true, we can take log4(n) to get the 
+     *   exponent (power) to which we would have to raise 4 to get n.
+     *   We can then check to see if the result is an integer. If it
+     *   is, n is a power of 4; otherwise, it is not.
+     * 
+     * PERFORMANCE:
+     * - Time Complexity: O(1) - Constant time b/c the log10() function
+     *                    will always run in O(1) and we have no loops.
+     * - Memory Complexity: O(1) - Only need to store a single constant.
+     *                      And even that is optional - doing so for
+     *                      readability.
+     */
     public boolean isPowerOfFour(int n) {
         
         double result = Math.log10(n) / Math.log10(4);
-        
-        System.out.println("result: " + result);
-        System.out.println("% 1: " + result % 1);
         
         return (result % 1) == 0;
     }
