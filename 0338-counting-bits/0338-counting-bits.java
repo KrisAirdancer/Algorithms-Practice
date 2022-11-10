fba7b394c6ec4c2e1015d4b8e1f112763fb06434
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-11-10 10:00:59 -0700

    Time: 8 ms (44.61%), Space: 48.2 MB (47.23%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,29 +2,36 @@ class Solution {
     
     /**
      * IDEA:
-     * - Use the left shift operator and bitwise OR operator to check if the
-     *   least significant bit is a 1. If it is, increment a counter.
-     * - Essentially, loop over the input value n, check the least significant
-     *   bit for a 1 useing |, then bit shift left once. Repeat.
+     * - Use the left shift operator and bitwise AND operator to check if the
+     *   least significant bit is a 1. If it is, increment a counter. When the
+     *   current value has been fully evaluated (it becomes 0), add the count
+     *   to the next open position in an array, clear the counter, and repeat.
      * 
      * PERFORMANCE:
-     * - Time Complexity:
-     * - Memory Complexity:
+     * - Time Complexity: O(NlogN) - Looping from 0 to n is linear, O(N). The number
+     *                    of bits in each of the valuse from 0 to n grows at 
+     *                    a logarithmic rate. So, for each additional N, where
+     *                    N == n, we loop an additional log(N) times. Therefore, 
+     *                    since we have an O(logN) loop inside an O(N) loop, we have
+     *                    an overall time complexity of O(logN) * O(N), which is
+     *                    just O(NlogN).
+     * - Memory Complexity: O(N) - Because we are storing an array of length n,
+     *                      we need O(N) space, where N == n. So, O(N).
      * 
+     * OUTLINE:
+     * - Instantiate int[n + 1] bitCounts
+     * - Loop from 0 to n (inclusive on both ends)
+     *     - Instantiate count = 0
+     *     - Loop while current > 0
+     *         - Use bitwise AND to check if the LSB bit is a 1 or 0
+     *         - If LSB is 1, count++
+     *         - Else, continue
+     *         -  Bit-shift to right by 1 bit
+     *     - Add count to bitCounts[i] (bitCounts[i] = count)
+     *     - Reset count to 0
+     * - Return bitCounts
      */
-    public int[] countBits(int n) {
-        
-        // Instantiate int[n + 1] bitCounts
-        // Loop from 0 to n (inclusive on both ends)
-            // Instantiate count = 0
-            // Loop while current > 0
-                // Use bitwise AND to check if the LSB bit is a 1 or 0
-                // If LSB is 1, count++
-                // Else, continue
-                // Bit-shift to right by 1 bit
-            // Add count to bitCounts[i] (bitCounts[i] = count)
-        // Return bitCounts
-        
+    public int[] countBits(int n) {    
         
         int[] bitCounts = new int[n + 1];
         bitCounts[0] = 0;
