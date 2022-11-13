2656bd01a852015f549cc121ecdf8bca9a3d4dc7
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-11-13 08:30:24 -0700

    Time: 95 ms (100.00%), Space: 41.7 MB (100.00%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.js
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -5,9 +5,9 @@
 var convertTemperature = function(celsius)
 {
     let results = [];
+    
     results.push(celsius + 273.15);
     results.push((celsius * 1.80) + 32.00);
-    console.log(results);
     
     return results;
 };
