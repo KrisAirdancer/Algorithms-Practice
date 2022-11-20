cebeec47c97c7d7341058cce589afae289f9cca3
Chris Marston <76403578+KrisAirdancer@users.noreply.github.com>
2022-11-20 09:12:08 -0700

    Time: 3 ms (29.45%), Space: 41.9 MB (16.16%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -0,0 +1,31 @@
+class Solution
+{
+    public int numIdenticalPairs(int[] nums)
+    {
+        // If seen before, increment count
+        
+        int count = 0;
+        
+        HashMap<Integer, Integer> seenNums = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < nums.length; i++)
+        {
+            int current = nums[i];
+            
+            if (seenNums.containsKey(current)) // Returns true if added; false if not added. THerefore, if false, we've seen the number before
+            {
+                int currentCount = seenNums.get(current);
+                
+                count += seenNums.get(nums[i]);
+                seenNums.put(current, currentCount + 1);
+                // count += seenNums.get(nums[i]);
+            }
+            else
+            {
+                seenNums.put(current, 1);
+            }
+        }
+        
+        return count;
+    }
+}
